dfc6119b5a85e178d7521efd2684df998771846c
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2025-01-28 12:24:43 +0700

    Menambahkan data class DataItem untuk menyimpan informasi tanaman

---
+++ app/src/main/java/com/example/tanipintar/model/DataItem.kt
@@ -0,0 +1,8 @@
+package com.example.tanipintar.model
+
+data class DataItem(
+    val id_tanaman: String,
+    val nama_tanaman: String,
+    val periode_tanam: String,
+    val deskripsi_tanaman: String
+)
